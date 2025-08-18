ca9ff3c7969b2cee739e6bf7832cd2fcc0696dc0
karansankrit01 <sankritkaran1@gmail.com>
2025-08-18 23:27:40 +0530

    App.css

---
+++ src/Components/BookNow.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BookNow = () => {
+  return (
+    <div>
+      <h2>Book Now</h2>
+      <p>Reserve your spot today!</p>
+    </div>
+  )
+}
+
+export default BookNow
